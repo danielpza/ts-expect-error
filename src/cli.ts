36d08a2a89197f2a2cbf2b977cf4f08b7575667b
Daniel Perez Alvarez <danielpza@protonmail.com>
2024-08-23 00:31:00 -0400

    feat: add --help flag

--- src/cli.ts
+++ src/cli.ts
@@ -12,12 +12,27 @@ const options = parseArgs({
       type: "boolean",
       short: "r",
     },
+    help: {
+      type: "boolean",
+      short: "h",
+    },
   },
 });
 
 const {
   positionals: [entry],
-  values: { "remove-current-checks": removeCurrentChecks },
+  values: { "remove-current-checks": removeCurrentChecks, help },
 } = options;
+
+if (help) {
+  console.log(`\
+ts-expect-error <glob> [...options]
+
+options
+  -h, --help                   Show this help message.
+  -r, --remove-current-checks  Remove previously placed @ts-expect-error directives.
+`);
+  process.exit(1);
+}
 
 await run({ entry, removeCurrentChecks });
